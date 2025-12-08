3d1281c08abcb8a65d80509db3c5514c611d1959
samira kibous <kiboussamira71@gmail.com>
2025-12-08 16:05:55 +0100

    R2-3 Créer le modèle User

---
+++ backend/models/User.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
+  role: { type: String, enum: ['admin','driver'], default: 'driver' },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', UserSchema);
