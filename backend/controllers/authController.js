257908fef39f1f49a9c48ff1fb0cc26f7e5ecfe0
samira kibous <kiboussamira71@gmail.com>
2025-12-09 12:03:43 +0100

    R2-5 logout

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -28,3 +28,10 @@ export const login = async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 };
+
+export const logout = (req, res) => {
+    res.status(200).json({
+        message: "Déconnexion réussie",
+        success: true
+    });
+};
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { login } from '../controllers/authController.js';
+import {logout} from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/login', login);
+router.post('/logout', logout);
 
 export default router;
